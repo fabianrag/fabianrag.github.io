cca4dd4c225b05c9108a56d4e4ad63e2664dd914
Fabian Araya <fbnarayaguerra@gmail.com>
2021-05-19 22:37:18 -0400

    Boton de logIn

---
+++ index.js
@@ -0,0 +1,40 @@
+//const fetchData = require("./src/utils/fetchData");
+const API = 'https://rickandmortyapi.com/api/character/';
+
+const  fetchData = (url_api) => {
+    return new Promise((resolve, reject) => {
+        const xhttp = new XMLHttpRequest();
+        xhttp.open('GET', url_api, true);
+        xhttp.onreadystatechange = (() => {
+          if(xhttp.readyState === 4){
+            (xhttp.status === 200)
+                ? resolve(JSON.parse(xhttp.responseText))
+                : reject(new Error("Error " + url_api)) 
+          }
+        });
+        xhttp.send();
+    });
+}
+
+const anotherFunction = async (url_api) => {
+    try {
+        const data = await fetchData(url_api);
+        const character = await fetchData(`${url_api}${data.results[0].id}`);
+        const origin = await fetchData(character.origin.url);
+        console.log(data.info.count);
+        console.log(character.name);
+        console.log(origin.dimension);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+console.log("Before");
+anotherFunction(API);
+console.log("After");
+
+const logIn = document.getElementById("logButton");
+const deployMessage = () => {
+    alert("Démole");
+}
+logIn.addEventListener("click", deployMessage);
