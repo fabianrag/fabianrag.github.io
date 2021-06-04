e764115691d1e69ea36067fd5fde27f45110ef4f
Fabian Araya <fbnarayaguerra@gmail.com>
2021-06-04 01:03:10 -0400

    Mejorando usuario

--- index.js
+++ index.js
@@ -33,17 +33,12 @@ console.log("Before");
 anotherFunction(API);
 console.log("After");
 /* Sección de log */
-const start = () => {
-    logBox.removeChild(logForm);
-    logBox.removeChild(registerForm);
-}
-const startLog = () => {
-    logBox.appendChild(logForm);
-}
-const deployLog = () => {
-    logBox.removeChild(registerForm);
-    logBox.appendChild(logForm);
-}
+
+
+//const deployLog = () => {
+//    logBox.removeChild(registerForm);
+//    logBox.appendChild(logForm);
+//}
 const deployRegister = () => {
     logBox.removeChild(logForm);
     logBox.appendChild(registerForm);
@@ -51,17 +46,46 @@ const deployRegister = () => {
 const anError = () => {
     alert("Aún falta agregar esta función");
 }
-const iniciarSesion = document.getElementById('iniciarSesion');
-const registrarme = document.getElementById('registrarme');
+
+const logSection_div = document.getElementsByClassName('logItem');
+const logSection_login_aButton = document.getElementById('iniciarSesion');
+const logSection_register_aButton = document.getElementById('registrarme');
 const returnLog = document.getElementById('returnLog');
 const returnRegister = document.getElementById('returnRegister');
-const logBox = document.getElementById('logBox');
-const logForm = document.getElementById("logForm");
-const registerForm = document.getElementById("registerForm");
-const logButton = document.getElementById("logButton");
-returnLog.addEventListener("click", deployLog);
+
+//returnLog.addEventListener("click", deployLog);
 returnRegister.addEventListener("click", deployRegister);
-logButton.addEventListener("click", startLog);
+
+//logSection_user_crossButton.addEventListener("click", startLog);
 iniciarSesion.addEventListener("click", anError);
 registrarme.addEventListener("click", anError);
+
+//Proceso de inicio de la página
+const logSection = document.getElementById('logSection');
+const logSection_user = document.getElementById('user');
+const logSection_user_crossButton = document.getElementById('logButton-cross');
+const logSection_login = document.getElementById("login");
+const logSection_register = document.getElementById("register");
+const start = () => {
+    logSection_user.removeChild(logSection_user_crossButton);
+    logSection.removeChild(logSection_login);
+    logSection.removeChild(logSection_register);
+}
 start();
+
+//Boton de Switch
+const logSection_user_switchButton = document.getElementById('logButton-switch');
+const deployLog = () => {
+    logSection.appendChild(logSection_login);
+    logSection_user.appendChild(logSection_user_crossButton);
+    logSection_user.removeChild(logSection_user_switchButton);
+}
+logSection_user_switchButton.addEventListener("click", deployLog);
+
+//Boton de cerrar
+const closeLog = () => {
+    logSection.removeChild(logSection_login);
+    logSection_user.appendChild(logSection_user_switchButton);
+    logSection_user.removeChild(logSection_user_crossButton);
+}
+logSection_user_crossButton.addEventListener("click", closeLog);
