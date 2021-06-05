ab648cb41ea372f6b1fde1fc77122ee5ad9830e1
Fabian Araya <fbnarayaguerra@gmail.com>
2021-06-05 00:37:59 -0400

    Boton switch y cross definitivos

--- index.js
+++ index.js
@@ -1,91 +1,22 @@
-//const fetchData = require("./src/utils/fetchData");
-const API = 'https://rickandmortyapi.com/api/character/';
+//userButton
+const userContainer = document.getElementById('user');
+const switchButton = document.getElementById('logButton-switch');
+const addSwitchButton = () => {
+    const crossButton = document.getElementById('logButton-cross');
+    userContainer.removeChild(crossButton);
+    userContainer.appendChild(switchButton);
+};
+const addCrossButton = () => {
+    const container = document.createElement('figure');
+    container.id = "logButton-cross";
 
-const  fetchData = (url_api) => {
-    return new Promise((resolve, reject) => {
-        const xhttp = new XMLHttpRequest();
-        xhttp.open('GET', url_api, true);
-        xhttp.onreadystatechange = (() => {
-          if(xhttp.readyState === 4){
-            (xhttp.status === 200)
-                ? resolve(JSON.parse(xhttp.responseText))
-                : reject(new Error("Error " + url_api)) 
-          }
-        });
-        xhttp.send();
-    });
-}
+    const image = document.createElement('img');
+    image.src = "/image/cross.png";
 
-const anotherFunction = async (url_api) => {
-    try {
-        const data = await fetchData(url_api);
-        const character = await fetchData(`${url_api}${data.results[0].id}`);
-        const origin = await fetchData(character.origin.url);
-        console.log(data.info.count);
-        console.log(character.name);
-        console.log(origin.dimension);
-    } catch (error) {
-        console.error(error);
-    }
-}
+    container.appendChild(image);
+    container.addEventListener('click', addSwitchButton);
 
-console.log("Before");
-anotherFunction(API);
-console.log("After");
-/* Sección de log */
-
-
-//const deployLog = () => {
-//    logBox.removeChild(registerForm);
-//    logBox.appendChild(logForm);
-//}
-const deployRegister = () => {
-    logBox.removeChild(logForm);
-    logBox.appendChild(registerForm);
-}
-const anError = () => {
-    alert("Aún falta agregar esta función");
-}
-
-const logSection_div = document.getElementsByClassName('logItem');
-const logSection_login_aButton = document.getElementById('iniciarSesion');
-const logSection_register_aButton = document.getElementById('registrarme');
-const returnLog = document.getElementById('returnLog');
-const returnRegister = document.getElementById('returnRegister');
-
-//returnLog.addEventListener("click", deployLog);
-returnRegister.addEventListener("click", deployRegister);
-
-//logSection_user_crossButton.addEventListener("click", startLog);
-iniciarSesion.addEventListener("click", anError);
-registrarme.addEventListener("click", anError);
-
-//Proceso de inicio de la página
-const logSection = document.getElementById('logSection');
-const logSection_user = document.getElementById('user');
-const logSection_user_crossButton = document.getElementById('logButton-cross');
-const logSection_login = document.getElementById("login");
-const logSection_register = document.getElementById("register");
-const start = () => {
-    logSection_user.removeChild(logSection_user_crossButton);
-    logSection.removeChild(logSection_login);
-    logSection.removeChild(logSection_register);
-}
-start();
-
-//Boton de Switch
-const logSection_user_switchButton = document.getElementById('logButton-switch');
-const deployLog = () => {
-    logSection.appendChild(logSection_login);
-    logSection_user.appendChild(logSection_user_crossButton);
-    logSection_user.removeChild(logSection_user_switchButton);
-}
-logSection_user_switchButton.addEventListener("click", deployLog);
-
-//Boton de cerrar
-const closeLog = () => {
-    logSection.removeChild(logSection_login);
-    logSection_user.appendChild(logSection_user_switchButton);
-    logSection_user.removeChild(logSection_user_crossButton);
-}
-logSection_user_crossButton.addEventListener("click", closeLog);
+    userContainer.appendChild(container);
+    userContainer.removeChild(switchButton);
+};
+switchButton.addEventListener('click', addCrossButton);
